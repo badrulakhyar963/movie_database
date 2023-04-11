c90dfe4c180060aa6a64b5691f5a63073fb6bc99
RisyadMF <badrulakhyar078@gmail.com>
2023-04-12 02:36:28 +0700

    praktikum 7

--- src/components/AddMovieForm/AddMovieForm.js
+++ src/components/AddMovieForm/AddMovieForm.js
@@ -1,7 +1,126 @@
+import { useState } from "react";
 import stayle from "./AddMovieForm.module.css"
+import { nanoid } from "nanoid";
 
 // Menangkap props
-function AddMovieForm() {
+function AddMovieForm(props) {
+  // destructing props
+  const {movies, setMovies } = props;
+
+  // membuat state title
+  const [title, setTitle] = useState("");
+  // membuat state date
+  const [date, setDate] = useState("");
+  // membuat state type
+  const [type, setType] = useState("");
+  // membuat state poster
+  const [poster, setPoster] = useState("");
+
+  // const [formData, setFormData] = useState([
+  //   {
+  //     title: "",
+  //     isTitleError: false,
+  //   },
+  //   {
+  //     date: "",
+  //     isDateError: false,
+  //   },
+  //   {
+  //     type: "",
+  //     isTypeError: false,
+  //   },
+  //   {
+  //     poster: "",
+  //     isPosterError: false,
+  //   },
+    
+  // ]);
+
+  // membuat state title erroe/empty
+  const [isTitleError, setIsTitleError] = useState(false);
+  // membuat state date erroe/empty
+  const [isDateError, setIsDateError] = useState(false);
+  // membuat state type erroe/empty
+  const [isTypeError, setIsTypeError] = useState(false);
+  // membuat state poster erroe/empty
+  const [isPosterError, setIsPosterError] = useState(false);
+
+  // function handleChange(e) {
+  //   // Destructing name dan value.
+  //   const { name, value } = e.target;
+
+    /**
+     * Mengupdate state berupa object:
+     * - Menggunakan spread operator:
+     * - Update property berdasarkan apapun nilai name.
+     */
+    // setFormData({
+    //   ...formData,
+    //   [name]: value,
+    // });
+  // }
+  // membuat function handleTitle
+  function handleTitle (e) {
+    setTitle(e.target.value);
+  }
+  // membuat fuction handleDate
+  function handleDate(e) {
+    setDate(e.target.value);
+  }
+  // membuat fuction handleType
+  function handleType(e) {
+    setType(e.target.value);
+  }
+  // membuat function handlePoster
+  function handlePoster(e) {
+    setPoster(e.target.value);
+  }
+  // membuat function HandleSubmit
+  function handleSubmit(e) {
+    // untuk mencegah perilaku default yaitu refresh menggunakan preventDefault
+    e.preventDefault();
+    // Jika title kosong maka set error title true
+    if (title === "") {
+      setIsTitleError(true);
+    }
+    // Jika date kosong maka set error title true
+    else if (date === "") {
+      setIsTitleError(false);
+      setIsDateError(true);
+    }
+    // Jika genre kosong maka set error title true
+    else if (type === "") {
+      setIsDateError(false);
+      setIsTypeError(true);
+    }
+    // Jika poster kosong maka set error title true
+    else if (poster === "") {
+      setIsTypeError(false);
+      setIsPosterError(true);
+    }
+    // Jika data diisi,lakukan tambah data
+    else {
+    // membuat data mevie yang akan digunakan
+    const movie = {
+      id: nanoid(8),
+      title: title,
+      year: date,
+      type: type,
+      poster: poster,
+    };
+    
+    // menambahkan movie ke state movies
+    // menggunakan spread operator untuk meng-copy dan merge array
+    setMovies([...movies, movie]);
+    setIsTitleError(false);
+    setIsDateError(false);
+    setIsTypeError(false);
+    setIsPosterError(false);
+    }
+  }
+  // const { title, date, poster, type } = formData;
+  // const {isTitleError,isDateError,isPosterError,isTypeError} = isError;
+
     return (
       <div className={stayle.container}>
         <section className={stayle.AddMovieForm}>
@@ -11,15 +130,48 @@ function AddMovieForm() {
 
           <div className={stayle.AddMovieForm__right}>
             <h2 className={stayle.AddMovieForm__title}>Add Movie Form</h2>
-            <form >
+
+            <form onSubmit={handleSubmit}>
               <div className={stayle.AddMovieForm__group}>
                 <label className={stayle.AddMovieForm__label}>Title</label>
-                <input className={stayle.AddMovieForm__input} type="text" name="title"/>
+                <input 
+                  onChange={handleTitle} 
+                  className={stayle.AddMovieForm__input} 
+                  type="text" 
+                  value={title}
+                />
+                {/* jika error title bernilai true maka akan muncul pesan error 
+                     jika tidak muncul string kosong
+                 */}
+                 {isTitleError && <alert>Title wajid diisi</alert>}
               </div>
 
               <div className={stayle.AddMovieForm__group}>
                 <label className={stayle.AddMovieForm__label}>Date</label>
-                <input className={stayle.AddMovieForm__input} type="text" name="date"/>
+                <input 
+                  onChange={handleDate} 
+                  className={stayle.AddMovieForm__input} 
+                  type="number" 
+                  value={date}
+                />
+                 { isDateError && <alert>Date wajid diisi</alert>}
+              </div>
+
+              <div className={stayle.AddMovieForm__group}>
+                <label className={stayle.AddMovieForm__label}>Genre</label>
+                <select onChange={handleType} className={stayle.AddMovieForm__input} value={type}>
+                  <option value="movie">Action</option>
+                  <option value="series">Drama</option>
+                  <option value="series">Horor</option>
+                  <option value="series">Comedy</option>
+                </select>              
+                { isTypeError && <alert>Genre wajid dipilih</alert>}
+              </div>
+
+              <div className={stayle.AddMovieForm__group}>
+                <label className={stayle.AddMovieForm__label}>Poster</label>
+                <input onChange={handlePoster} className={stayle.AddMovieForm__input} type="url" value={poster}/>
+                { isPosterError && <alert>link poster wajid diisi</alert>}
               </div>
 
               <div>
--- src/components/movies/Movies.js
+++ src/components/movies/Movies.js
@@ -1,47 +1,36 @@
+import Movie from "../movie/movie";
 import stayle from "./Movies.module.css";
+import { nanoid } from "nanoid";
 
-function Movies(){
+function Movies(props){
+    // destructing props
+    const {movies, setMovies } = props;
+
+    function tambahFilm() {
+      const movie = {
+        id: nanoid(8),
+        title: "Fast Saga",
+        year: 2023,
+        type: "Movie",
+        poster: "https://picsum.photos/300/400",
+      };
+    //   menambahkan movie ke state movies
+    // menggunakan spread operator untuk meng-copy dan merge array
+      setMovies([...movies, movie]);
+    }
+
     return(
         <div className={stayle.container}>
             <section className={stayle.movies}>
                 <h2 className={stayle.movies__title}>lates Movie</h2>
                 <div className={stayle.movie__container}>
-                <div className={stayle.movie}>
-                        <img className={stayle.movie__image} src="https://upload.wikimedia.org/wikipedia/en/thumb/9/9c/Detective_Knight_Independence_poster.png/220px-Detective_Knight_Independence_poster.png" alt=""></img>
-                        <h3 className={stayle.movie__title}>Detective Knight: Independence</h3>
-                        <p className={stayle.movie__date}>2023</p>
-                    </div>
-                    
-                    <div className={stayle.movie}>
-                        <img className={stayle.movie__image} src="https://upload.wikimedia.org/wikipedia/id/thumb/5/54/Avatar_The_Way_of_Water_poster.jpg/220px-Avatar_The_Way_of_Water_poster.jpg" alt=""></img>
-                        <h3 className={stayle.movie__title}>Avatar: The Way of Water</h3>
-                        <p className={stayle.movie__date}>2022</p>
-                    </div>
-
-                    <div className={stayle.movie}>
-                        <img className={stayle.movie__image} src="https://upload.wikimedia.org/wikipedia/id/thumb/3/3b/Black_Panther_Wakanda_Forever_poster.jpg/220px-Black_Panther_Wakanda_Forever_poster.jpg" alt=""></img>
-                        <h3 className={stayle.movie__title}>Black Panther: Wakanda Forever</h3>
-                        <p className={stayle.movie__date}>2022</p>
-                    </div>
-
-                    <div className={stayle.movie}>
-                        <img className={stayle.movie__image} src="https://upload.wikimedia.org/wikipedia/en/thumb/7/75/The_Man_from_Toronto_%282022_film%29.jpg/220px-The_Man_from_Toronto_%282022_film%29.jpg" alt=""></img>
-                        <h3 className={stayle.movie__title}>The Man from Toronto</h3>
-                        <p className={stayle.movie__date}>2022</p>
-                    </div>
-                    
-                    <div className={stayle.movie}>
-                        <img className={stayle.movie__image} src="https://upload.wikimedia.org/wikipedia/en/thumb/c/ce/JurassicWorldDominion_Poster.jpeg/220px-JurassicWorldDominion_Poster.jpeg" alt=""></img>
-                        <h3 className={stayle.movie__title}>Jurassic World Dominion</h3>
-                        <p className={stayle.movie__date}>2022</p>
-                    </div>
-
-                    <div className={stayle.movie}>
-                        <img className={stayle.movie__image} src="https://upload.wikimedia.org/wikipedia/id/0/0d/Avengers_Endgame_poster.jpg" alt=""></img>
-                        <h3 className={stayle.movie__title}>Avengers: Endgame</h3>
-                        <p className={stayle.movie__date}>2019</p>
-                    </div>
+                    {
+                        movies.map(function(movie) {
+                            return <Movie key={movie.id} movie={movie}/>
+                        })
+                    }
                 </div>
+                <button onClick={tambahFilm}>Add Movie</button>
             </section>
         </div>
     );
