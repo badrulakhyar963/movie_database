a1dfdc8202d9aee0054e8d5a1212002c19c0a570
RisyadMF <badrulakhyar078@gmail.com>
2023-06-09 20:01:33 +0700

    praktikum 12

--- src/components/movie/movie.js
+++ src/components/movie/movie.js
@@ -7,9 +7,13 @@ function Movie(props) {
 
     return(
         <StyledMovie>
-            <img src={movie.poster} alt=""></img>
+            <img 
+            src={
+                movie.poster || 
+                `https://image.tmdb.org/t/p/w300/${movie.poster_path }` 
+                } alt=""/>
             <h3>{movie.title}</h3>
-            <p>{movie.year}</p>
+            <p>{movie.year || movie.release_date}</p>
         </StyledMovie>
     )
 }
--- src/pages/movie/Popular.js
+++ src/pages/movie/Popular.js
@@ -1,10 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Movies from "../../components/movies/Movies";
+import Hero from "../../components/hero/Hero";
 
-function Popular() {
+function PopularMovie() {
+ const [movies,setMovies] = useState([]);
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const URL =`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`;
+
+  async function fetchPopularMovie() {
+    const response = await axios(URL);
+    console.log(response)
+
+    setMovies(response.data.results);
+  }
+  useEffect(function (){
+    fetchPopularMovie();
+
+  },[]);
+
     return (
     <div>
-        <h2>popular Movie</h2>
+      <Hero/>
+      <Movies movies={movies} setMovies={setMovies} />
     </div>
   );
 }
 
-export default Popular;
+export default PopularMovie;
